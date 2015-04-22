0c865e23e5625eb70d2692336f6a475ce00b4d0e
diogofreitaspe <diogolaco@gmail.com>
2015-04-22 19:52:32 -0300

    removendo arquivos deletados

--- test.rb
+++
@@ -1,7 +0,0 @@
-ignored_users = ['CABELTRAM', 'MateusNoronha', 'deborahcalacia', 'arianeprado', 'TahZanin', 'viniciuscarvalho', 'BrunaMM']
-
-if not ignored_users.include?('CABELTRAM')
-	puts "xxxx"
-else
-	puts "yyyy"
-end
