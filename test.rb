d49178b38d0bcfc73ef0791b3db733d34944238b
diogofreitaspe <diogolaco@gmail.com>
2015-04-22 14:12:55 -0300

    criando repo

---
+++ test.rb
@@ -0,0 +1,7 @@
+ignored_users = ['CABELTRAM', 'MateusNoronha', 'deborahcalacia', 'arianeprado', 'TahZanin', 'viniciuscarvalho', 'BrunaMM']
+
+if not ignored_users.include?('CABELTRAM')
+	puts "xxxx"
+else
+	puts "yyyy"
+end
